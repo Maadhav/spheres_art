1136847c3ff86915bd05d8d76e9e2711529ef682
Maadhav Sharma <maadhav2001@gmail.com>
2021-08-29 23:25:30 +0530

    play video on hover

--- app/src/components/card/NFTCard.js
+++ app/src/components/card/NFTCard.js
@@ -1,19 +1,62 @@
-import React, { useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
-import './NFTCard.css'
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import HoverVideoPlayer from "react-hover-video-player";
+
+import "./NFTCard.css";
 
 const NFTCard = ({ sphere }) => {
-    const location = useLocation()
-    const history = useHistory()
-    const [ipfsCid, ipfsName] = sphere.image.split('ipfs://')[1].split('/')
-    return (
-        <div className="container" onClick={() => { history.push({ pathname: (location.pathname === "/" ? "" : location.pathname) + `/item/${sphere.token_id}`, state: sphere }) }}>
+  const location = useLocation();
+  const history = useHistory();
+  const [thumbnailCid, thumbnailName] = sphere.image
+    .split("ipfs://")[1]
+    .split("/");
+//   const [previewCid, previewName] = sphere.image.split("ipfs://")[1].split("/");
+  return (
+    <div
+      className="container"
+      onClick={() => {
+        history.push({
+          pathname:
+            (location.pathname === "/" ? "" : location.pathname) +
+            `/item/${sphere.token_id}`,
+          state: sphere,
+        });
+      }}
+    >
 
-            <img src={`https://ipfs.io/ipfs/${ipfsCid}/${ipfsName}`} className="image-container" alt="" />
-            <div className="title-style">{sphere.name}</div>
-            <div className="price-style"><span style={{ fontWeight: "600" }}>{(sphere.price / 1000000).toFixed(2)}</span> XTZ</div>
-        </div>
-    )
-}
+      <HoverVideoPlayer
+        videoSrc={`https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`}
+        loop={true}
+        sizingMode="container"
+        videoStyle = {{
+            height: "211px",
+            marginBottom: "15px",
+            borderRadius: "20px"
+        }}
+        unloadVideoOnPaused={true}
+        style={{
+            height: "211px",
+            marginBottom: "15px",
+            // borderRadius: "20px"
+        }}
+        pausedOverlay={
+          <img
+            src={`https://ipfs.io/ipfs/${thumbnailCid}/${thumbnailName}`}
+            className="image-container"
+            alt=""
+          />
+        }
+        loadingOverlay={"Loading"}
+      />
+      <div className="title-style">{sphere.name}</div>
+      <div className="price-style">
+        <span style={{ fontWeight: "600" }}>
+          {(sphere.price / 1000000).toFixed(2)}
+        </span>{" "}
+        XTZ
+      </div>
+    </div>
+  );
+};
 
-export default NFTCard
+export default NFTCard;
