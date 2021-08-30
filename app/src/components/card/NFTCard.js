e072ddf68e284a9f5104eb4a679c134253886adf
Maadhav Sharma <maadhav2001@gmail.com>
2021-08-30 13:45:42 +0530

    improved loading time for NFT card

--- app/src/components/card/NFTCard.js
+++ app/src/components/card/NFTCard.js
@@ -1,16 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import HoverVideoPlayer from "react-hover-video-player";
 
 import "./NFTCard.css";
+import getIPFSData from "../../adapters/ipfs";
 
-const NFTCard = ({ sphere }) => {
+const NFTCard = ({ sphere, onLoadIPFS }) => {
   const location = useLocation();
   const history = useHistory();
-  const [thumbnailCid, thumbnailName] = sphere.image
-    .split("ipfs://")[1]
-    .split("/");
-  const [previewCid, previewName] = sphere.properties.preview.split("ipfs://")[1].split("/");
+
+  const [ipfsData, setIpfsData] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [preview, setPreview] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  const loadData = async () => {
+    console.log(sphere)
+    let ex = await getIPFSData(sphere.tokenUrl.split("ipfs://")[1]);
+    let ipfsData = JSON.parse(ex);
+    onLoadIPFS({ ...sphere, ...ipfsData });
+    setIpfsData(ipfsData);
+    setThumbnail({
+      cid: ipfsData.image.split("ipfs://")[1].split("/")[0],
+      name: ipfsData.image.split("ipfs://")[1].split("/")[1],
+    });
+    setPreview({
+      cid: ipfsData.properties.preview.split("ipfs://")[1].split("/")[0],
+      name: ipfsData.properties.preview.split("ipfs://")[1].split("/")[1],
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div
       className="container"
@@ -19,45 +43,50 @@ const NFTCard = ({ sphere }) => {
           pathname:
             (location.pathname === "/" ? "" : location.pathname) +
             `/item/${sphere.token_id}`,
-          state: sphere,
+          state: { ...sphere, ...ipfsData },
         });
       }}
     >
-
-      <HoverVideoPlayer
-        videoSrc={`https://ipfs.io/ipfs/${previewCid}/${previewName}`}
-        loop={true}
-        sizingMode="overlay"
-        overlayTransitionDuration ={0}
-        videoStyle = {{
-            height: "211px",
-            marginBottom: "15px",
-            borderRadius: "20px"
-        }}
-        unloadVideoOnPaused={true}
-        style={{
-            height: "211px",
-            marginBottom: "15px",
-            // borderRadius: "20px"
-        }}
-        pausedOverlayWrapperStyle={{
-          zIndex: "0"
-        }}
-        pausedOverlay={
-          <img
-            src={`https://ipfs.io/ipfs/${thumbnailCid}/${thumbnailName}`}
-            className="image-container"
-            alt=""
+      {!  isLoading ? (
+        <>
+          <HoverVideoPlayer
+            videoSrc={`https://ipfs.io/ipfs/${preview.cid}/${preview.name}`}
+            loop={true}
+            sizingMode="overlay"
+            overlayTransitionDuration={0}
+            videoStyle={{
+              height: "211px",
+              marginBottom: "15px",
+              borderRadius: "20px",
+            }}
+            unloadVideoOnPaused={true}
+            style={{
+              height: "211px",
+              marginBottom: "15px",
+              // borderRadius: "20px"
+            }}
+            pausedOverlayWrapperStyle={{
+              zIndex: "0",
+            }}
+            pausedOverlay={
+              <img
+                src={`https://ipfs.io/ipfs/${thumbnail.cid}/${thumbnail.name}`}
+                className="image-container"
+                alt=""
+              />
+            }
           />
-        }
-      />
-      <div className="title-style">{sphere.name}</div>
-      <div className="price-style">
-        <span style={{ fontWeight: "600" }}>
-          {(sphere.price / 1000000).toFixed(2)}
-        </span>{" "}
-        XTZ
-      </div>
+          <div className="title-style">{ipfsData.name}</div>
+          <div className="price-style">
+            <span style={{ fontWeight: "600" }}>
+              {(sphere.price / 1000000).toFixed(2)}
+            </span>{" "}
+            XTZ
+          </div>{" "}
+        </>
+      ) : (
+        "Loading"
+      )}
     </div>
   );
 };
--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,55 +1,79 @@
-import { start } from 'ipfs-core/src/components/network'
-import React, { useEffect, useState } from 'react'
-import getIPFSData from '../adapters/ipfs'
-import { getContractStorage } from '../adapters/tezos'
-import LinedButton from '../components/button/LinedButton'
-import NFTCard from '../components/card/NFTCard'
-import Loader from '../components/loader/Loader'
-import './HomePage.css'
+import { start } from "ipfs-core/src/components/network";
+import React, { useEffect, useState } from "react";
+import { getContractStorage } from "../adapters/tezos";
+import LinedButton from "../components/button/LinedButton";
+import NFTCard from "../components/card/NFTCard";
+import Loader from "../components/loader/Loader";
+import "./HomePage.css";
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+  const [spheres, setSpheres] = useState([]);
+  const [length, setLength] = useState(0);
+  async function getData() {
+    var data = (await getContractStorage()).spheres.valueMap;
+    var parseData = Array.from(data)
+      .map((k, v) => k[1])
+      .filter((e) => e.isNew);
+    let spheres = [];
+    for (let i = 0; i < parseData.length; i++) {
+      const sphere = parseData[i];
 
-    const [loading, setLoading] = useState(true)
-    const [spheres, setSpheres] = useState([])
-    const [length, setLength] = useState(0)
-    async function getData() {
-        var data = (await getContractStorage()).spheres.valueMap;
-        var spheres = [];
-        var parseData = (Array.from(data).map((k, v) => k[1]))
-        for (let i = 0; i < parseData.length; i++) {
-            const sphere = parseData[i];
-            if (sphere.isNew) {
-                let ex = await getIPFSData(sphere.tokenUrl.split('ipfs://')[1])
-                let ipfsData = JSON.parse(ex);
-                spheres.push({ ...sphere, ...ipfsData, });
-            }
-        }
-        setSpheres(spheres)
-        setLength(8)
-        setLoading(false)
+      spheres.push(sphere);
     }
-    useEffect(() => {
-        getData()
-    }, [])
-    return (
-        <div className="home-body">
-            <div className="discover-card">
-                <div className="circle-1"></div>
-                <div className="circle-2"></div>
-                <span>Discover, collect, and sell extraordinary Sphere NFTs</span>
-            </div>
-            <div className="explore-section">
-                <h1>Explore</h1>
-                {loading ? <Loader/>:
-                <div className="nft-grid">
-                    {
-                        spheres.map((e,i) => i < length && <NFTCard sphere={e} key={e.token_id} />)
+    console.log(spheres)
+    setSpheres(spheres);
+    setLength(8);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="home-body">
+      <div className="discover-card">
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <span>Discover, collect, and sell extraordinary Sphere NFTs</span>
+      </div>
+      <div className="explore-section">
+        <h1>Explore</h1>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="nft-grid">
+            {spheres.map(
+              (e, i) =>
+                i < length && (
+                  <NFTCard
+                    sphere={e}
+                    key={e.token_id}
+                    onLoadIPFS={
+                        (sphere) => 
+                      setSpheres((val) => 
+                        val.map((e, index) => {
+                          if (index == i) return sphere;
+                          else return e;
+                        })
+                      )
                     }
-                </div>}
-            </div>
-            {spheres.length > length  && <LinedButton title="Load More" onClick={() => { setLength(val => val + 8) }} style={{ marginBottom: "60px", width: "300px" }} />}
-        </div>
-    )
-}
+                  />
+                )
+            )}
+          </div>
+        )}
+      </div>
+      {spheres.length > length && (
+        <LinedButton
+          title="Load More"
+          onClick={() => {
+            setLength((val) => val + 8);
+          }}
+          style={{ marginBottom: "60px", width: "300px" }}
+        />
+      )}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
