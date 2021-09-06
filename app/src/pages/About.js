4a984c769682f78a304bf991d1f56df40b44594e
kunal528 <64953335+kunal528@users.noreply.github.com>
2021-09-06 08:00:07 +0530

    download button remove from about page

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -156,9 +156,9 @@ const About = () => {
                     <SolidButton title="Online Editor" style={{ fontSize: "20px", height: "auto" }} onClick={() => {
                         window.open("https://sphereart-editor.netlify.app/editor/", "_blank", "", true).focus();
                     }} />
-                    <LinedButton title="Download" style={{ fontSize: "20px", height: "auto" }} onClick={() => {
+                    {/* <LinedButton title="Download" style={{ fontSize: "20px", height: "auto" }} onClick={() => {
                         window.open("https://github.com/Maadhav/sphere-art-three.js/archive/refs/heads/master.zip", "_blank", "", true).focus();
-                    }} />
+                    }} /> */}
                 </div>
                 <div className="video-container"></div>
                 <div className="title-text-style" style={{ marginTop: "135px" }}>Features</div>
@@ -186,49 +186,3 @@ const About = () => {
 
 export default About
 
-
-// <mesh
-//     visible // object gets render if true
-//     userData={{ test: "hello" }} // An object that can be used to store custom data about the Object3d
-//     position={[0, 0, 1]} // The position on the canvas of the object [x,y,x]
-//     rotation={[0, 0, 0]} // The rotation of the object
-//     castShadow // Sets whether or not the object cats a shadow
-//     // There are many more props.....
-//   >
-//     {/* A spherical shape*/}
-//     <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-//     {/* A standard mesh material*/}
-//     D"
-//       attach="material"
-//       distort={0.5} // Strength, 0 disables the effect (default=1)
-//       speed={2} // Speed (default=1)
-//       roughn<meshStandardMaterial
-// //       attach="material" // How the element should attach itself to its parent
-//       color="#7222D3" // The color of the material
-//       transparent // Defines whether this material is transparent. This has an effect on rendering as transparent objects need special treatment and are rendered after non-transparent objects. When set to true, the extent to which the material is transparent is controlled by setting it's .opacity property.
-//       roughness={0.1} // The roughness of the material - Defaults to 1
-//       metalness={0.1} // The metalness of the material - Defaults to 0
-//     />
-//   </mesh>
-//   {/*An ambient light that creates a soft light against the object */}
-//   <ambientLight intensity={0.5} />
-//   {/*An directional light which aims form the given position */}
-//   <directionalLight position={[10, 10, 5]} intensity={1} />
-//   {/*An point light, basically the same as directional. This one points from under */}
-//   <pointLight position={[0, -10, 5]} intensity={1} />
-//   {/* We can use the drei Sphere which has a simple API. This sphere has a wobble material attached to it */}
-//   <Sphere visible position={[-3, 0, 1]} args={[1, 16, 16]}>
-//     <MeshWobbleMaterial
-//       attach="material"
-//       color="#EB1E99"
-//       factor={1} // Strength, 0 disables the effect (default=1)
-//       speed={2} // Speed (default=1)
-//       roughness={0}
-//     />
-//   </Sphere>
-//   {/* This sphere has a distort material attached to it */}
-//   <Sphere visible position={[3, 0, 1]} args={[1, 16, 16]}>
-//     <MeshDistortMaterial
-//       color="#00A38ess={0}
-//     />
-//   </Sphere> */}
