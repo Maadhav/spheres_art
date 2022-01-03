a4bdc374b4ff4d12fa34f501f2086df08edad8ec
kunal528 <64953335+kunal528@users.noreply.github.com>
2022-01-03 12:15:49 +0530

    loading fixed in item page

--- app/src/pages/ItemPage.js
+++ app/src/pages/ItemPage.js
@@ -6,21 +6,21 @@ import Payment from '../components/dialog/Payment'
 import Blockies from 'react-blockies'
 import { APP } from '../adapters/three.js/index'
 import { useLocation, useParams } from 'react-router-dom'
-import Loader from '../components/loader/Loader'
-import { getActiveAccount, getContractStorage, updatePrice } from '../adapters/tezos'
-import getIPFSData from '../adapters/ipfs'
+import { getActiveAccount, updatePrice } from '../adapters/tezos'
 import { EditSquare, TickSquare } from 'react-iconly'
 import { DatabaseService } from '../adapters/firebase'
 import { limit, orderBy, query, where } from 'firebase/firestore'
+import axios from 'axios'
 const ItemPage = (props) => {
     const location = useLocation()
     const [checkout, setCheckout] = useState(false)
     const [payment, setPayment] = useState(false)
     const [state, setstate] = useState(props.location.state)
     const [loading, setLoading] = useState(true)
+    const [progress, setProgress] = useState(0);
     const [editing, setEditing] = useState(false)
     const [wallet, setWallet] = useState()
-    const [price, setPrice] = useState(((props?.location?.state?.price ?? 0)/ 1000000).toFixed(2))
+    const [price, setPrice] = useState(((props?.location?.state?.price ?? 0) / 1000000).toFixed(2))
 
 
     const { id } = useParams()
@@ -32,12 +32,12 @@ const ItemPage = (props) => {
             var data = await DatabaseService.get({
                 col: 'spheres',
                 query: (ref) => {
-                    return query(ref,orderBy('timestamp', 'desc'), where('token_id', '==', parseInt(id)), limit(1))
+                    return query(ref, orderBy('timestamp', 'desc'), where('token_id', '==', parseInt(id)), limit(1))
                 }
             })
             nft = data[0];
             setstate(nft);
-            setPrice((nft.price/ 1000000).toFixed(2)) 
+            setPrice((nft.price / 1000000).toFixed(2))
         }
         else {
             nft = state;
@@ -48,21 +48,26 @@ const ItemPage = (props) => {
             redirect: 'follow'
         };
 
-        fetch(nft.app, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                let ipfsData = JSON.parse(result);
-                var player = new APP.Player();
-                player.load(ipfsData);
+
+        axios.get(nft.app, {
+            onDownloadProgress: (progressEvent) => {
+                console.log(progressEvent);
+                console.log(progressEvent.loaded / progressEvent.total);
+                setProgress(progressEvent.loaded / progressEvent.total);
+            },
+        }).then(response => {
+            var player = new APP.Player();
+            player.load(response.data);
+            player.setSize(542, 542);
+            player.play();
+            window.addEventListener('resize', function () {
                 player.setSize(542, 542);
-                player.play();
-                window.addEventListener('resize', function () {
-                    player.setSize(542, 542);
-                });
-                setLoading(false)
-                ref.current.appendChild(player.dom);
-            })
-            .catch(error => console.log('error', error));
+            });
+            setLoading(false)
+            ref.current.appendChild(player.dom);
+        }).catch(error => {
+            console.log(error);
+        })
     }
 
     const activeWallet = async () => {
@@ -92,18 +97,27 @@ const ItemPage = (props) => {
         init()
         activeWallet()
     }, [])
-    if (loading)
-        return <Loader />
     return (
         <div className="section-separator">
-            <div className="image-section" ref={ref}>
-                {loading && <Loader />}
-            </div>
+            {loading ? <div className='image-section'>
+                <img className='image-loading-display padding' src={state.image} alt={state.image} />
+                <div className='overlay'>
+                    <div className='overlay-content'>
+                        <div className='overlay-content-text'>
+                            Loading 3D Rendering
+                        </div>
+                        <progress value={progress} max={1}></progress>
+                    </div>
+                </div>
+            </div> :
+                <div className="image-section" ref={ref}>
+                </div>
+            }
             <div className="details-section">
                 <div style={{ margin: "45px 43px" }}>
                     <h1>{state.title}</h1>
                     <div style={{ display: 'flex', alignItems: "center", gap: "0 10px" }}>
-                        {editing ? <input className="price-field-input" onChange={(e) => setPrice(e.target.value)} value={price} /> : <div className="price-text">{price } XTZ</div>}
+                        {editing ? <input className="price-field-input" onChange={(e) => setPrice(e.target.value)} value={price} /> : <div className="price-text">{price} XTZ</div>}
                         {(state.creator === wallet?.address && state.creator === state.owner) && (editing ? <TickSquare set="bold" primaryColor="white" size={20} onClick={onPriceEdit} /> : <EditSquare set="bold" size={20} primaryColor="white" onClick={() => { setEditing(true) }} />)}
                     </div>
                     <div className="creator">Creator</div>
@@ -141,7 +155,7 @@ const ItemPage = (props) => {
             </div>
             {checkout && <CheckOut sphere={state} onQuit={() => { setCheckout(false) }} onCheckOut={() => { setPayment(true); setCheckout(false) }} />}
             {payment && <Payment sphere={state} onQuit={() => { setPayment(false) }} />}
-        </div>
+        </div >
     )
 }
 
