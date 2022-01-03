b9d04f08023d7e57c2efd93947a3777d4c0f2b09
Maadhav Sharma <maadhav2001@gmail.com>
2022-01-03 18:14:27 +0530

    implemented validations on create page

--- app/src/pages/CreateItem.js
+++ app/src/pages/CreateItem.js
@@ -2,51 +2,99 @@ import React, { useEffect, useState } from "react";
 import DragDrop from "../components/dragdrop/DragDrop";
 import SolidButton from "../components/button/SolidButton";
 import "./CreateItem.css";
-import { createItem, uploadToIPFS, confirmOperation, getContractStorage } from "../adapters/tezos";
-import JSZip from 'jszip'
+import {
+  createItem,
+  uploadToIPFS,
+  confirmOperation,
+  getContractStorage,
+} from "../adapters/tezos";
+import JSZip from "jszip";
 import SphereCanvas from "../components/loader/SphereCanvas";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { StorageService, DatabaseService } from '../adapters/firebase/index'
+import { StorageService, DatabaseService } from "../adapters/firebase/index";
 
 const CreateItem = () => {
   const [loading, setLoading] = useState(false);
   const [title, setTitle] = useState("");
+  const [errors, setErrors] = useState({});
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [file, setFile] = useState(null);
 
   useEffect(() => {
-    console.log(file);
-  }, [file]);
+    console.log(errors);
+  }, [errors]);
 
   async function onCreate() {
+    setErrors({});
+    if (!file) {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      toast.error("Please upload a file");
+      return;
+    } else if (title.length < 3 || description.length < 3 || price <= 0) {
+      console.log("Test");
+      if (title.length < 3) {
+        setErrors((errors) => {
+          return {
+            ...errors,
+            title: "Title must be at least 3 characters",
+          };
+        });
+        console.log("Error");
+      }
+      if (description.length < 3) {
+        setErrors((errors) => {
+          return {
+            ...errors,
+            description: "Description must be at least 3 characters",
+          };
+        });
+      }
+      if (price <= 0) {
+        setErrors((errors) => {
+          return {
+            ...errors,
+            price: "Price must be more than 0",
+          };
+        });
+      }
+      toast.error("Please enter all fields correctly");
+      document.body.scrollTop = 200; // For Safari
+      document.documentElement.scrollTop = 200; // For Chrome, Firefox, IE and Opera
+      return;
+    }
     setLoading(true);
     let imageFile, jsonFile, videoFile;
-    var entries = []
-    await JSZip.loadAsync(file).then((zip) => {
-      zip.forEach((e, entry) => {
-        entries.push(entry)
-      })
-    })
+    var entries = [];
+    try {
+      await JSZip.loadAsync(file).then((zip) => {
+        zip.forEach((e, entry) => {
+          entries.push(entry);
+        });
+      });
+    } catch (e) {
+      toast.error("Please upload a valid zip file");
+    }
     for (let index = 0; index < entries.length; index++) {
       const entry = entries[index];
-      var blob = await entry.async('blob')
-      blob.name = entry.name
+      var blob = await entry.async("blob");
+      blob.name = entry.name;
       if (entry.name === "app.json") jsonFile = blob;
       else if (entry.name === "thumbnail.png") imageFile = blob;
       else videoFile = blob;
     }
     if (!jsonFile) {
-      alert("Zip doesn't contain App.json");
+      toast.error("Zip doesn't contain App.json");
     } else if (!imageFile) {
-      alert("Zip doesn't contain Thumbnail");
+      toast.error("Zip doesn't contain Thumbnail");
     } else if (!videoFile) {
-      alert("Zip doesn't contain Preview Video");
+      toast.error("Zip doesn't contain Preview Video");
     } else if (
       JSON.parse(await jsonFile.text()).metadata.source !== "sphere.ART Editor"
     ) {
-      alert("Export from Sphere.ART Editor");
+      toast.error("Export from Sphere.ART Editor");
     } else {
       const url = await toast.promise(
         uploadToIPFS({
@@ -67,7 +115,7 @@ const CreateItem = () => {
         createItem({
           price: price * 1000000,
           url: url,
-          title: title
+          title: title,
         }),
         {
           pending: "Minting NFT to Blockchain",
@@ -81,30 +129,41 @@ const CreateItem = () => {
         error: "Operation rejected 🤯",
       });
       var data = (await getContractStorage()).spheres.valueMap;
-      console.log(data)
+      console.log(data);
       var sphere = Array.from(data)
-      .map((k, v) => k[1]).find(e => e.tokenUrl === url)
+        .map((k, v) => k[1])
+        .find((e) => e.tokenUrl === url);
       console.log(sphere);
-      var downloadUrls = await toast.promise(StorageService.upload(`spheres/${sphere.token_id}`, [imageFile, videoFile, jsonFile]), {
-        pending: "Uploading Files to Central Storage",
-        success: "Upload Complete",
-        error: "Upload rejected 🤯",
-      })
+      var downloadUrls = await toast.promise(
+        StorageService.upload(`spheres/${sphere.token_id}`, [
+          imageFile,
+          videoFile,
+          jsonFile,
+        ]),
+        {
+          pending: "Uploading Files to Central Storage",
+          success: "Upload Complete",
+          error: "Upload rejected 🤯",
+        }
+      );
       console.log(downloadUrls);
-      var sphereData = await toast.promise(DatabaseService.set('spheres', {
-        ...sphere,
-        description: description,
-        price: sphere.price.c[0],
-        token_id: sphere.token_id.c[0],
-        timestamp: sphere.timestamp.c[0],
-        image: downloadUrls[0],
-        preview: downloadUrls[1],
-        app: downloadUrls[2],
-      }), {
-        pending: "Uploading Data to Central Server",
-        success: "Upload Complete",
-        error: "Upload rejected 🤯",
-      })
+      var sphereData = await toast.promise(
+        DatabaseService.set("spheres", {
+          ...sphere,
+          description: description,
+          price: sphere.price.c[0],
+          token_id: sphere.token_id.c[0],
+          timestamp: sphere.timestamp.c[0],
+          image: downloadUrls[0],
+          preview: downloadUrls[1],
+          app: downloadUrls[2],
+        }),
+        {
+          pending: "Uploading Data to Central Server",
+          success: "Upload Complete",
+          error: "Upload rejected 🤯",
+        }
+      );
       console.log(sphereData);
       setFile(null);
       setDescription("");
@@ -155,32 +214,83 @@ const CreateItem = () => {
             <input
               placeholder="Item Name"
               className="field-input"
-              onChange={(e) => setTitle(e.currentTarget.value)}
+              onChange={(e) => {
+                setTitle(e.currentTarget.value);
+                setErrors((errors) => {
+                  delete errors.title;
+                  return { ...errors };
+                });
+              }}
               value={title}
+              style={{
+                border: errors.title ? "1px solid red" : "",
+              }}
             />
+            <span
+              style={{
+                color: errors.title ? "red" : "",
+              }}
+            >
+              {errors.title}
+            </span>
           </div>
           <div className="field-section">
             <h2>Description</h2>
             <textarea
               placeholder="Description of the Item"
               className="field-textarea"
-              onChange={(e) => setDescription(e.currentTarget.value)}
+              onChange={(e) => {
+                setDescription(e.currentTarget.value);
+                setErrors((errors) => {
+                  delete errors.description;
+                  return { ...errors };
+                });
+              }}
               value={description}
-            ></textarea>
+              style={{
+                border: errors.description ? "1px solid red" : "",
+              }}
+            />
+            <span
+              style={{
+                color: errors.description ? "red" : "",
+              }}
+            >
+              {errors.description}
+            </span>
           </div>
           <div className="field-section">
             <h2>Price</h2>
-            <div className="price-field">
+            <div
+              className="price-field"
+              style={{
+                border: errors.price ? "1px solid red" : "",
+              }}
+            >
               <input
                 placeholder="Enter Price"
                 className="price-input"
-                onChange={(e) => setPrice(e.currentTarget.value)}
+                type="number"
+                onChange={(e) => {
+                  setPrice(e.currentTarget.value);
+                  setErrors((errors) => {
+                    delete errors.price;
+                    return { ...errors };
+                  });
+                }}
                 value={price}
               />
               <select className="crypto-drop-down">
                 <option value="XTZ">XTZ</option>
               </select>
             </div>
+            <span
+              style={{
+                color: errors.price ? "red" : "",
+              }}
+            >
+              {errors.price}
+            </span>
           </div>
           <div
             style={{
