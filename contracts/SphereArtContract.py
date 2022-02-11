156ad56b827f46ccaa2c1a732ff9b7bbfa893d65
Maadhav Sharma <maadhav2001@gmail.com>
2022-02-11 18:01:35 +0530

    Update SphereArtContract.py

--- contracts/SphereArtContract.py
+++ contracts/SphereArtContract.py
@@ -1,117 +1,118 @@
 import smartpy as sp
-import time
+
+# Import FA2 template
 FA2 = sp.io.import_template("FA2.py")
 
-class NFT(FA2.FA2):
+class Token(FA2.FA2):
+    pass
+
+
+class SphereArt(sp.Contract):
+    def __init__(self, token, metadata, admin):
+        self.init(
+            token = token,
+            metadata = metadata,
+            admin = admin,
+            spheres = sp.big_map(tkey=sp.TNat, tvalue=sp.TRecord(token_id=sp.TNat, owner=sp.TAddress, creator = sp.TAddress, price=sp.TNat, isNew=sp.TBool, tokenUrl=sp.TString, timestamp=sp.TInt, title=sp.TString)),
+            token_id = 0,
+            )
 
     @sp.entry_point
     def mint(self, params):
-        if self.config.non_fungible:
-            sp.verify(
-                ~ self.token_id_set.contains(self.data.all_tokens, params.token_id),
-                message = "NFT-asset: cannot mint twice same token"
-            )
-        user = self.ledger_key.make(params.creator, params.token_id)
-        self.token_id_set.add(self.data.all_tokens, params.token_id)
-        sp.if self.data.ledger.contains(user):
-            self.data.ledger[user].balance += 1
-        sp.else:
-            self.data.ledger[user] = FA2.Ledger_value.make(1)
-        sp.if self.data.token_metadata.contains(params.token_id):
-            if self.config.store_total_supply:
-                self.data.total_supply[params.token_id] = 1
-        sp.else:
-            self.data.token_metadata[params.token_id] = sp.record(
-                token_id    = params.token_id,
-                token_info  = params.metadata
-            )
-            if self.config.store_total_supply:
-                self.data.total_supply[params.token_id] = 1
+        sp.verify((params.sphere.price > 0))
+        c = sp.contract(
+            sp.TRecord(
+            address=sp.TAddress,
+            amount=sp.TNat,
+            token_id=sp.TNat,
+            metadata=sp.TMap(sp.TString, sp.TBytes)
+            ), 
+            self.data.token, 
+            entry_point = "mint").open_some()
+            
+        sp.transfer(
+            sp.record(
+            address = sp.self_address,
+            amount = 1,
+            token_id = self.data.token_id,
+            metadata= self.data.metadata,
+            ),
+            sp.mutez(0), 
+            c)
+        
+        self.data.spheres[self.data.token_id] = sp.record(token_id = self.data.token_id, owner = sp.sender, creator = sp.sender, price = params.sphere.price, tokenUrl = params.sphere.tokenUrl, isNew = True, timestamp = params.sphere.timestamp, title = params.sphere.title)
+        self.data.token_id += 1
+    
+    @sp.entry_point
+    def collect_management_rewards(self, params):
+        sp.verify(sp.sender == self.data.admin)
+
+        sp.send(params.address, params.amount)
 
     @sp.entry_point
-    def single_transfer(self, params):
-        current_from = params.from_
-        if self.config.single_asset:
-            sp.verify(params.token_id == 0, message = "single-asset: token-id <> 0")
-        sp.verify(
-            self.data.token_metadata.contains(params.token_id),
-            message = self.error_message.token_undefined()
-        )
-        from_user = self.ledger_key.make(current_from, params.token_id)
-        sp.verify(
-            (self.data.ledger[from_user].balance >= 1),
-            message = self.error_message.insufficient_balance())
-        to_user = self.ledger_key.make(params.to_, params.token_id)
-        self.data.ledger[from_user].balance = sp.as_nat(
-            self.data.ledger[from_user].balance - 1)
-        sp.if self.data.ledger.contains(to_user):
-            self.data.ledger[to_user].balance += 1
-        sp.else:
-                self.data.ledger[to_user] = FA2.Ledger_value.make(1)
-                
+    def collect(self, params):
+        sp.verify(((sp.amount == sp.utils.nat_to_mutez(self.data.spheres[params.token_id].price)) & (self.data.spheres[params.token_id].price != 0) & (self.data.spheres[params.token_id].isNew == True) & (self.data.spheres[params.token_id].creator != sp.sender)))
+        self.data.spheres[params.token_id].isNew = False
+        self.data.spheres[params.token_id].owner = sp.sender
 
-class SphereArt(sp.Contract):
-    def __init__(self, contract_address):
-        self.init(spheres = {},counter = 0, contract_address = contract_address)
+        #sending rewards
+        sp.send(self.data.spheres[params.token_id].creator, sp.split_tokens(sp.amount, 93, 100))
+        
+        self.fa2_transfer(self.data.token, sp.self_address, sp.sender, params.token_id, 1)
 
     @sp.entry_point
-    def createItem(self, params):
-        sp.set_type(params.sphere.tokenUrl, sp.TString)
-        sp.set_type(params.sphere.price, sp.TNat)
-        sp.set_type(params.sphere.timestamp, sp.TInt)
-        sp.verify(0 <= params.sphere.price)
-        sp.trace(params.sphere)
-        token_contract = sp.contract(sp.TRecord(creator = sp.TAddress, metadata = sp.TMap(sp.TString, sp.TBytes),token_id = sp.TNat ), self.data.contract_address, entry_point = "mint").open_some()
-        sp.transfer(sp.record(creator = sp.sender, token_id = self.data.counter, metadata = FA2.FA2_token_metadata.make_metadata(
-        decimals = 0,
-        name = 'Sphere.ART Token',
-        symbol = 'SPHERE'
-        )), sp.mutez(0), token_contract)
-        self.data.spheres[self.data.counter] = sp.record(token_id = self.data.counter, owner = sp.sender, creator = sp.sender, price = sp.utils.nat_to_mutez(params.sphere.price), tokenUrl = params.sphere.tokenUrl, isNew = True, timestamp = params.sphere.timestamp, title = params.sphere.title)
-        self.data.counter+=1
-   
-    @sp.entry_point
-    def createSale(self, params):
+    def update(self, params):
         sphere = self.data.spheres[params.token_id]
-        sp.verify(sp.amount == sphere.price)
-        sp.send(sphere.owner, sp.amount)
-        sphere.owner = sp.sender
-        token_contract = sp.contract(sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, token_id = sp.TNat), self.data.contract_address, entry_point = "single_transfer").open_some()
-        sp.transfer(sp.record(from_ = sphere.creator, to_ = sp.sender, token_id = params.token_id), sp.mutez(0), token_contract)
-        sp.if sphere.isNew:
-            sphere.isNew = False
+        sp.verify((sp.sender == sphere.creator) & (sphere.isNew))
+        sphere.price = params.price
 
     @sp.entry_point
-    def updatePrice(self, params):
+    def delete(self, params):
         sphere = self.data.spheres[params.token_id]
-        sp.verify(sp.sender == sphere.creator)
-        sp.verify(sphere.isNew)
-        sphere.price = sp.utils.nat_to_mutez(params.price)
-
-    @sp.entry_point
-    def deleteItem(self, params):
-        sphere = self.data.spheres[params.token_id]
-        sp.verify(sp.sender == sphere.creator)
-        sp.verify(sphere.isNew)
+        sp.verify((sp.sender == self.data.admin) & (sphere.isNew))
         del self.data.spheres[params.token_id]
 
-@sp.add_test(name = "a")
-def test():
-    admin = sp.address("tz1bm9dFuBnSzTzgZKuHjJsFfrPfdkVgj1PW")
-    mark = sp.test_account("Mark")
-    elon = sp.test_account("Elon")
-    scenario  = sp.test_scenario()
-    scenario.h1("Spheres Art")
-    nft = NFT(FA2.FA2_config(non_fungible = True), admin = admin, metadata = sp.big_map({"": sp.utils.bytes_of_string("tezos-storage:content"),"content": sp.utils.bytes_of_string("""{"name" : "Sphere.ART FA2 Contract", "author": "CodeDecoders", "status": "In Development"}""")}))
-    scenario += nft
-    c1 = SphereArt(nft.address)
-    scenario += c1  
+    @sp.entry_point
+    def update_admin(self, params):
+        sp.verify(sp.sender == self.data.admin)
+        self.data.admin = params
+        
+    def fa2_transfer(self, fa2, from_, to_, token_id, amount):
+        c = sp.contract(sp.TList(sp.TRecord(from_=sp.TAddress, txs=sp.TList(sp.TRecord(amount=sp.TNat, to_=sp.TAddress, token_id=sp.TNat).layout(("to_", ("token_id", "amount")))))), fa2, entry_point='transfer').open_some()
+        sp.transfer(sp.list([sp.record(from_=from_, txs=sp.list([sp.record(amount=amount, to_=to_, token_id=token_id)]))]), sp.mutez(0), c)
 
+
+
+@sp.add_test(name = "Non Fungible Token")
+def test():
+    scenario = sp.test_scenario()
+    
+    admin = sp.test_account("admin")
+    mark = sp.test_account("user1")
+    elon = sp.test_account("user2")
     def newSphere(price, tokenUrl,title):
         return sp.record(price = sp.nat(price), tokenUrl = tokenUrl, timestamp = 1629614520,title = title)
-    c1.createItem(sphere = newSphere(1000, "test.com",'Testing')).run(sender = mark)
+    
+    token_contract = Token(FA2.FA2_config(non_fungible = True), admin = admin.address, metadata = sp.big_map({"": sp.utils.bytes_of_string("tezos-storage:content"),"content": sp.utils.bytes_of_string("""{"name" : "Sphere.ART FA2 Contract", "author": "CodeDecoders", "status": "In Development"}""")}))
+    
+    scenario += token_contract
 
-    c1.updatePrice(token_id = 0, price = 2000).run(sender = mark)
-    c1.deleteItem(token_id = 0).run(sender = mark)
-    c1.createItem(sphere = newSphere(1000, "test.com",'Testing 2')).run(sender = mark)
-# sp.add_compilation_target("sphereArt", SphereArt())
+    scenario.h1("MarketPlace")
+    marketplace = SphereArt(token_contract.address,  FA2.FA2_token_metadata.make_metadata(
+                decimals = 0,
+                name = 'Sphere.ART Token',
+                symbol = 'SPHERE'
+                ), admin.address)
+    scenario += marketplace
+    scenario.h1("Mint")
+    scenario += token_contract.set_administrator(marketplace.address).run(sender = admin)
+    scenario += marketplace.mint(sphere = newSphere(10000, "test2.com",'Testing')).run(sender = admin)
+    scenario += marketplace.mint(sphere = newSphere(30000, "test.com",'Testing2')).run(sender = mark)
+    scenario.h1("Collect")
+    scenario += marketplace.collect(sp.record(token_id = 0)).run(sender = elon, amount = sp.mutez(10000))
+    scenario.h1("Update")
+    scenario += marketplace.update(token_id = 1, price = 40000).run(sender = mark)
+    scenario.h1("Delete")
+    scenario += marketplace.delete(token_id = 1).run(sender = admin)
+
+    scenario += marketplace.collect_management_rewards(sp.record(amount = sp.mutez(700), address = admin.address)).run(sender = admin)
